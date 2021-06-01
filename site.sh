e8a68cd4df21867088e6f66a66c48294c1f75da8
LutsenkoVO <lutsenko.v.o@gmail.com>
2021-06-01 07:34:36 +0000

    first file

---
+++ site.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Start backup"
+date
+
+TAR=`which tar`
+
+syst_dir=/home/backups/site
+
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir -p ${syst_dir}/`date +%Y-%m`/
+  fi
+  
+  tar -czf ${syst_dir}/`date +%Y-%m`/backup-day-$(date +%Y-%m-%d-%H%M%S).tar.gz --listed-incremental=${syst_dir}/`date +%Y-%m`/sites.snapshot /home/wordpress
+
+date
+echo "Finish backup"
